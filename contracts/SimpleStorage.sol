38d47e993bfc2464df5cb8598d51a7ad6b339dd7
iamade <kamar3deen@gmail.com>
2024-11-24 12:23:50 -0700

    store fave number

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.24; // Solidity version
+
+contract SimpleStorage{
+    uint256 myFavoriteNumber; // 0
+
+    //uint256[]
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    //dynamic array
+    Person[] public listOfPeople; //[]
+
+    //chelsea -> 232
+    mapping(string => uint256) public nameToFavoriteNumber; 
+
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    //view, pure
+    function retrieve() public view returns(uint256){
+        return myFavoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public{
+        listOfPeople.push(Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+
+}
